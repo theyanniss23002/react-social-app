c31dc8f32a52005e2ca7ad8329d1454676274c79
The Yanniss <theyanniss@MacBook-Pro-The.local>
2021-11-21 17:22:16 +0300

    First push and cleared assembly for next work

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
-import ruLocale from 'date-fns/locale/ru';
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@mui/styles';
 import './styles/common.scss';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-
-const theme = createTheme({
-    palette: {
-        secondary: {
-            main: '#51a351',
-            contrastText: '#fff'
-        }
-    }
-});
 
 const App = () => {
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns} locale={ruLocale}>
-            <ThemeProvider theme={theme}>
-                <div className='wrap'>
-                    <span>REACT ASSEMBLY</span>
-                </div>
-            </ThemeProvider>
-        </LocalizationProvider>
+        <div className='wrap'>
+            <span>REACT ASSEMBLY</span>
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import App from './App';
 import './styles/common.scss';
 import { Provider, ReactReduxContext } from 'react-redux';
 import { store } from './store';
-import CssBaseline from '@mui/material/CssBaseline';
 
 render(
     <Provider store={store} context={ReactReduxContext}>
-        <CssBaseline />
         <App />
     </Provider>,
     document.getElementById('root')
